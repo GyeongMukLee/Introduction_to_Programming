2dbb7656193384575de51be5a9247abd17bc6e8d
Lee GyeongMuk <moogi3469@gmail.com>
2018-10-01 19:05:20 +0900

    주석 추가

--- 20180911/180911실습_성적 출력하기.c
+++ 20180911/180911실습_성적 출력하기.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 
 /*
-2018년 09월 11일 프로그래밍입문 실습과제
+2018년 09월 11일
+한경대학교 프로그래밍입문 실습과제
 
 학생의 성적과 평균을 출력하는 프로그램
 */
+
 int main() {
 	double math, korean, history, average;
 	int student_no=2013109; 
@@ -18,8 +20,12 @@ int main() {
 	printf("%-6s : %5.0f점\n", "국어", korean);
 	printf("%-6s : %5.0f점\n", "세계사", history);
 	printf("%-6s : %5.0f점\n\n", "수학", math);
-	
 	printf("%-6s : %5.2f점", "평균", average);
+	
+	/*
+	%-6s는 string형 자료를 6자리까지(한글은 한 글자가 두자리를 차지한다)표시하고, 왼쪽 정렬한다는 의미이다.
+	%5.0f는 float형 자료를 5자리까지 표시하고, 소수점 0자리까지 나타낸다는 의미이다.
+	*/
 	
 	return 0;
 }
