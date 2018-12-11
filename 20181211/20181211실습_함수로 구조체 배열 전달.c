106b55876d53026d24a972a43910c62dda7a1869
Lee GyeongMuk <moogi3469@gmail.com>
2018-12-11 11:19:27 +0900

    Add files via upload

--- 20181211/20181211실습_함수로 구조체 배열 전달.c
+++ 20181211/20181211실습_함수로 구조체 배열 전달.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
 
+struct member{
+	char name[10];
+	char gender[3];
+	int age;
+};
+
+void call_by_adress(struct member *buyer);
+void call_by_value(struct member buyer[]);
+
+int main(){
+	struct member buyer[3]={{"김철수", "남", 10}, {"배철수", "남", 65}, {"안철수", "남", 56}};
+	
+	call_by_adress(&buyer);
+	printf("\n");
+	call_by_value(buyer);
+	
+	return 0;
+}
+
+void call_by_adress(struct member *buyer){
+	int i;
+	printf(">> Call By Adress\n");
+	printf("%6s %6s %6s\n","이름","성별","나이");
+	printf("=================================\n");
+	
+	for(i=0;i<3;i++){
+		printf("%6s %6s %6d\n",buyer[i].name,buyer[i].gender,buyer[i].age);		//집가서 확인  
+	}
+}
+
+void call_by_value(struct member buyer[]){
+	int i;
+	printf(">> Call By Value\n");
+	printf("%6s %6s %6s\n","이름","성별","나이");
+	printf("=================================\n");
+	
+	for(i=0;i<3;i++){
+		printf("%6s %6s %6d\n",buyer[i].name,buyer[i].gender,buyer[i].age);
+	}
+}
